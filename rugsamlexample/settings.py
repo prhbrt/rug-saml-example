8c6870d2cb20e76f5e40028bb04cd88f98c2457a
Herbert Kruitbosch <H.T.Kruitbosch@rug.nl>
2017-10-27 13:41:28 +0200

    database

--- rugsamlexample/settings.py
+++ rugsamlexample/settings.py
@@ -48,12 +48,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'rugsamlexample.wsgi.application'
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'rugsamlexample',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 SAML2_AUTH = {
